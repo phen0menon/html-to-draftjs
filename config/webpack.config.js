e01bf2dd23e49677ca92346b090086d14f867851
Rolan Ibragimov <phen0menon@Rolans-MacBook-Air.local>
2021-12-27 13:34:22 +0300

    fix window issue

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -9,31 +9,32 @@ module.exports = {
     path: path.join(__dirname, "../dist"),
     filename: "html-to-draftjs.js",
     library: "htmlToDraftjs",
-    libraryTarget: "umd"
+    libraryTarget: "umd",
+    globalObject: "this",
   },
   optimization: {
-    minimizer: [new UglifyJsPlugin()]
+    minimizer: [new UglifyJsPlugin()],
   },
   externals: {
     "draft-js": "draft-js",
-    immutable: "immutable"
+    immutable: "immutable",
   },
   plugins: [
     new webpack.DefinePlugin({
       "process.env": {
-        NODE_ENV: JSON.stringify("production")
-      }
-    })
+        NODE_ENV: JSON.stringify("production"),
+      },
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: [{ loader: "babel-loader" }]
-      }
-    ]
+        use: [{ loader: "babel-loader" }],
+      },
+    ],
   },
   resolve: {
-    extensions: [".js", ".json"]
-  }
+    extensions: [".js", ".json"],
+  },
 };
